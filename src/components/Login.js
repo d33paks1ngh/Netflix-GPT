1b5beb832b2614859f649d22cd4f573fd616d538
Deepak Singh <ds8853845715@gmail.com>
2024-12-12 21:35:58 +0530

    sign Up added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import { LOGIN_PAGE_BG_IMG } from "../utils/constants";
 
 const Login = () => {
+  const [signInToggle, SetsignInToggle] = useState(true);
+  const handle_sign_in_toggle = () => {
+    SetsignInToggle(!signInToggle);
+  };
   return (
-    <div className="flex relative h-screen">
+    <div className="flex relative h-screen  bg-black">
       <Header />
       <img className="w-full" src={LOGIN_PAGE_BG_IMG} alt="bg-img" />
       <div className="absolute bottom-0 top-24 mx-auto  inset-0 w-4/12  bg-black text-white p-8 border-2 border-black rounded-xl bg-opacity-70">
         <form className="w-8/12 mx-auto ">
-          <h1 className="font-bold text-2xl m-2 p-2">Sign In</h1>
+          <h1 className="font-bold text-2xl m-2 p-2">
+            {signInToggle ? "Sign In" : "Sign Up"}
+          </h1>
+          {!signInToggle && (
+            <input
+              className="bg-opacity-70 border-2 border-slate-400 w-full m-2 p-2  rounded-l bg-black"
+              type="text"
+              placeholder="Enter Full Name"
+            />
+          )}
+
           <input
             className="bg-opacity-70 border-2 border-slate-400 w-full m-2 p-2  rounded-l bg-black"
             type="text"
@@ -21,19 +35,36 @@ const Login = () => {
             placeholder="Password"
           />
           <button className="w-full m-2 p-2 rounded-xl bg-red-600">
-            Sign In
+            {signInToggle ? "Sign In" : "Sign Up"}
           </button>
-          <p className="text-light text-slate-800 text-center w-full p-2">OR</p>
-          <button className="w-full mx-2 p-2 rounded-xl bg-gray-400 bg-opacity-70">
-            Use a sign-in code
-          </button>
-          <p className="text-light text-center w-full  p-2">Forget Password?</p>
-          <input id="first" type="checkbox" className="m-2 p-2" />
-          <label for="first" class="ms-2 text-sm font-medium">
-            Remember me
-          </label>
+          {signInToggle && (
+            <p className="text-light text-slate-400 text-center w-full p-2">
+              OR
+            </p>
+          )}
+          {signInToggle && (
+            <button className="w-full mx-2 p-2 rounded-xl bg-gray-400 bg-opacity-70">
+              Use a sign-in code
+            </button>
+          )}
+
+          {signInToggle && (
+            <p className="text-light text-center w-full  p-2">
+              Forget Password?
+            </p>
+          )}
+          {signInToggle && (
+            <input id="first" type="checkbox" className="m-2 p-2" />
+          )}
+          {signInToggle && <label htmlFor="first">Remember me</label>}
           <p className="text-light text-left w-full  p-2">
-            New to Netflix?<spam>Sign up now</spam>
+            {signInToggle ? "New to Netflix ? " : "Already registered ? "}
+            <span
+              className="cursor-pointer hover:text-blue-600"
+              onClick={handle_sign_in_toggle}
+            >
+              {signInToggle ? "Sign Up" : "Sign in Now."}
+            </span>
           </p>
         </form>
       </div>
